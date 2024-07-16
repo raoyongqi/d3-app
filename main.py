41b2b7f2be7886f4224762e23decdc3616d0c587
raoyongqi <220220934821@lzu.edu.cn>
2024-07-16 12:54:24 +0800

    0701603

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to your needs
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/data")
+async def get_data():
+    with open('volcano.json') as f:
+        data = json.load(f)
+    return data
